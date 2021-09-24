88353e545b280ec89e68270c262f373df75fc09b
Apochen <15309036477@qq.com>
2021-09-24 16:53:45 +0800

    Removed the bug label.

--- app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
@@ -109,7 +109,7 @@ public class PlaceRenderer extends Renderer<Place> {
         final View.OnClickListener listener = view12 -> {
 
             /** Themis-#3244 */
-            Log.i("Themis-#3244", "Step 3: Selected a place in the nearby list.");
+            Log.i("Themis", "Step 3: Selected a place in the nearby list.");
             /** Themis-#3244 */
 
             Timber.d("Renderer clicked");
@@ -155,7 +155,7 @@ public class PlaceRenderer extends Renderer<Place> {
             } else {
 
                 /** Themis-#3244 */
-                Log.i("Themis-#3244", "Step 4: Clicked the \"Camera\" in nearby list item menu. If photo is taken, the crash will occur.");
+                Log.i("Themis", "Step 4: Clicked the \"Camera\" in nearby list item menu. If photo is taken, the crash will occur.");
                 /** Themis-#3244 */
 
                 Timber.d("Camera button tapped. Image title: " + place.getName() + "Image desc: " + place.getLongDescription());
